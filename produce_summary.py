c8763df01d0d19541f055e331786e8618a9a8753
eycanny <nancy.the.coder@gmail.com>
2022-03-16 19:06:09 -0700

    Added comments explaining each line of code

--- produce_summary.py
+++ produce_summary.py
@@ -4,15 +4,15 @@ def generate_sales_report(day, file):
     Print the day of the report.
     Print individual lines containing the amount delivered, what was delivered, and total amount.
     """
-    print(f"Day {day}")
-    the_file = open(file)
-    for line in the_file:
-        line = line.rstrip()
-        words = line.split('|')
+    print(f"Day {day}") #print message with day passed in as an argument
+    the_file = open(file) #opens file passed in as an argument
+    for line in the_file: #iterates thru each line in the file defined above
+        line = line.rstrip() #removes whitespace from the right
+        words = line.split('|') #use | as a delimiter when tokenizing line
 
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
+        melon = words[0] #grabs first value of list and assign it to variable
+        count = words[1] #grabs second value of list and assign it to variable
+        amount = words[2] #grabs third value of list and assign it to variable
 
-        print(f"Delivered {count} {melon}s for total of ${amount}")
-    the_file.close()
+        print(f"Delivered {count} {melon}s for total of ${amount}") #print a message with f-string formatting with the above defined variables
+    the_file.close() #closes file
