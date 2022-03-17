a428190d068444516c86d09ef05e47cad3820ae0
eycanny <nancy.the.coder@gmail.com>
2022-03-16 19:01:26 -0700

    Refactored code to be more readable and less repetitive by creating generate_sales_report function

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,18 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def generate_sales_report(day, file):
+    """Print a line-by-line sales report based on desired day's file.
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+    Print the day of the report.
+    Print individual lines containing the amount delivered, what was delivered, and total amount.
+    """
+    print(f"Day {day}")
+    the_file = open(file)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
